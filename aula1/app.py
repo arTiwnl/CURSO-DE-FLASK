450f492c141eec86d69cd9203fe185fc161d5ba2
Arthur Carvalho <57073690+arTiwnl@users.noreply.github.com>
2022-01-04 11:30:48 -0300

    Add files via upload

---
+++ aula1/app.py
@@ -0,0 +1,17 @@
+# Aula 01 - Instalando Flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Index"
+
+@app.route("/teste")
+def teste():
+    return "Teste"
+
+if __name__ == '__main__':
+    app.run();
