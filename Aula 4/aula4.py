06f5094cb72c42006a8258890715027168c0f757
Arthur Carvalho <57073690+arTiwnl@users.noreply.github.com>
2022-01-08 15:52:00 -0300

    # Aula 4 - Construção de URL

---
+++ Aula 4/aula4.py
@@ -0,0 +1,25 @@
+# Aula 4 - Construção de URL
+
+from flask import Flask, redirect, url_for
+
+app = Flask (__name__)
+
+@app.route ("/admin")
+def admin():
+    return "<h1>Admin</h1>"
+
+
+@app.route ("/guest/<guest>")
+def guest(guest):
+    return '<p>olá guest <b>%s</b></p>' %guest
+
+@app.route ("/user/<name>")
+def user(name):
+    if name == "admin":
+        return redirect(url_for('admin'))
+    else:
+        return redirect(url_for('guest', guest=name))
+
+
+if __name__  == '__main__' :
+    app.run(debug=True)
